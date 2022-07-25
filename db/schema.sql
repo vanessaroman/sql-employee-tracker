267f2cbe8c5ea563bf763dcbaee01a7229a1458b
vanessaroman <vanessavirginiarr@gmail.com>
2022-07-25 08:14:56 -0600

    schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,28 @@
 -- Database Creation
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  INDEX (name)
+);
+
+CREATE TABLE employee_role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department VARCHAR(30),
+    FOREIGN KEY (department) REFERENCES department(name)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,16 @@
 -- tables here
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO employee_role (id, title, salary, department)
+VALUES ("201", "Sales Lead", "100000", "Sales"),
+       ("202", "Salesperson", "80000", "Sales"),
+       ("203", "Lead Engineer", "150000", "Engineering"),
+       ("204", "Software Engineer", "120000", "Engineering"),
+       ("204", "Account Manager", "160000", "Finance"),
+       ("204", "Accountant", "125000", "Finance"),
+       ("204", "Legal Team Lead", "250000", "Legal"),
+       ("204", "Lawyer", "190000", "Legal");
